8de316192e5cd708ecbe1825ad2fcd07bc495842
CHANDAN <kumardalaichandan803@gmail.com>
2025-12-02 22:57:55 +0530

    Add files via upload

---
+++ DAY02/second.cpp
@@ -0,0 +1,213 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    // int a ,b ,sum;
+    // cout <<"Enter ur number: ";
+    // cin>>a;
+    // cout <<"Enter ur number: ";
+    // cin>>b;
+    // sum = a+b;
+    // cout<<sum;
+
+//    char c = 'a'+1; 97+1 => 98 >> c
+    //   cout<<c;
+//    int c = 'a'+1;  // 97+1 = 98
+//    cout<<c;
+   
+    // //Arithmatic operator 
+    // int a = 20;
+    // int b = 10;
+    // cout<<a+b;
+    // cout<<endl;  //end of line > endl
+    // cout<<a-b<<endl;
+    // cout<<a*b<<endl;
+    // cout<<a/b<<endl;
+    // cout<<a%b<<endl;
+
+
+    // assignment operator  
+    // a+=b; //a = a+b
+    // a-=b; //a = a-b
+    // cout<<a;
+    // comparision operator 
+    //< > <= >= !=
+    // int a = 10 ;
+    // int b = 10 ;
+    // cout<<(a>=b);
+    // cout<<(a<=b);
+    // cout<<(a!=b);
+    
+
+    // int a = 20 ;
+    // //left shift 
+    // // 20 * 2 ^3   >>  20 in binary move point left side 
+    // a = a<<3;
+    // cout<<a;
+
+//    cout<<endl;
+
+    // int b = 40 ;
+    // //right shift 
+    // // 20 / 2 ^3
+    // b = b>>3;
+    // cout<<b;
+
+
+
+    //logical operator 
+    // cout<<(true && true)<<endl;
+
+    // cout<<(true && 8 )<<endl;
+    // cout<<(true || false );
+
+ 
+
+    // cout<<(!0);
+
+
+
+
+    // int a = 10;
+    // int b;
+    // //pre increment ++a increment a then performe function  , post increment a++ >> use a then function 
+    // //pre decrement 
+    // //post decrement  
+    // // int b = a++;
+    // // cout<<b;
+    // // cout<<a;
+    // b = a--;
+    // cout <<b<<endl;
+    // cout<<a;
+
+
+
+
+ //bitwise operator 
+ // 10 > 1010
+ // 5 > 0101   
+ // compare bit by bit at binary level 
+//     int a = 10;
+//     int b = 5;
+//     cout<<( a & b)<<endl;
+//     cout<<( a | b)<<endl;
+//     cout<<( a ^ b)<<endl;  //>>
+//   cout <<endl;
+//     cout<<(0 ^ 0)<<endl; 
+//     cout<<(0 ^ 1)<<endl; 
+//     cout<<(1 ^ 0)<<endl; 
+//     cout<<(1 ^ 1)<<endl; 
+
+
+    // int age ;
+    // cout<<"Enter your age : ";
+    // cin>>age;
+
+    // if(age>18){
+    //     cout<<"You are eligible for vote";
+    // }
+    // else{
+    //     cout<<"You are not eligible for vote";
+    // }
+
+
+    // int marks;
+    // cout<<"Enter your marks: ";
+    // cin>>marks;
+
+    // if (marks>90){
+    //     cout<<"Your grade is A+";
+    // }
+    // else if (marks>80 && marks <= 90)
+    // {
+    //     cout<<"Your grade is A";
+    // }
+    // else if (marks>70 && marks <=80){
+    //     cout<<"Your grade is B";
+    // }
+    // else if (marks>60 && marks <=70)
+    // {
+    //     /* code */cout<<"Grade C";
+    // }
+    // else{
+    //     cout<<"Grade is D";
+    // }
+    
+  
+
+    // int day; 
+    // cout<<"Enter your day; ";
+    // cin>>day;
+    // switch (day)
+    // {
+    // case 1 :
+    //     cout<<"Monday";
+    //     break;
+    // case 2 :
+    //     cout<<"Tuesday";
+    //     break;
+    // case 3 :
+    //     cout<<"Wednesday";
+    //     break;
+    // case 4 :
+    //     cout<<"Thursday";
+    //     break;
+    // case 5 :
+    //     cout<<"Friday";
+    //     break;
+    // case 6 :
+    //     cout<<"Saturday";
+    //     break;
+    // case 7:
+    //     cout<<"Sunday"    ;
+    //     break;
+    
+    // default:{
+    //     cout<<"Not a valid day sojjaa bhai";
+    // }
+    //     break;
+    // }
+    
+
+
+
+    // int num ;
+    // cout<<"Enter your number : ";
+    // cin>>num;
+    // if (num%2==0) cout<<"Even number";
+    // else cout<<"Odd number ";
+
+    // int num1 , num2;
+    // cout<<"Enter your number : ";
+    // cin>>num1;
+    // cout<<"Enter your number : ";
+    // cin>>num2;
+    // if(num1>num2) 
+    // {
+    //     cout<<"Num1 is greater ";
+    // }
+    // else if (num1<num2) {
+    //     cout <<"Num2 is greater ";
+    // }
+    // else  {
+    //     cout <<"Num1  Num2";
+    // }
+
+
+    // loops 
+
+    // for (int i = 1; i < 800 ; i ++){
+    //     cout<<"Chandan is a Topper Student "<<endl;
+    // }
+
+
+
+    //  for (int i = 1 ; i <= 23 ; i++){
+    //     cout<<i;
+    //  }
+
+
+     return 0;
+}
