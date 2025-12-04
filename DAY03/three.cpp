1f8192f8d1be18463a40fea6d7046d3c5db91bdb
CHANDAN <kumardalaichandan803@gmail.com>
2025-12-04 23:12:51 +0530

    Add files via upload
    not complete code ok

---
+++ DAY03/three.cpp
@@ -0,0 +1,133 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+//even number
+//     for (int i = 0 ; i<=20 ; i=i+2){
+//     cout<<i<<endl;
+// }
+
+//odd number
+//     for (int i = 1 ; i<=20 ; i=i+2){
+//     cout<<i<<endl;
+// }
+
+//alphabate 
+// for (char alpha = 'a' ; alpha<= 'z' ; alpha++ ){
+//     cout<<alpha<<endl;
+// }
+
+
+//sum of first 10 num 
+// int sum = 0;
+// for (int i = 1 ; i <= 10 ; i++){
+//     sum = sum+i;
+// }
+// cout<<sum;
+
+//mul able 
+// int num = 5 ;
+// for (int i = 0; i <= 10 ; i++){
+//     cout<<num << "X" <<i <<"=" <<(num*i)<<endl;
+// }
+
+//sum of square 
+// int sum = 0;
+// for (int i = 1 ; i <= 10 ; i++){
+//     sum = sum+(i*i);
+// }
+// cout<<sum;
+
+// for (int j = 1 ; j <= 6 ; j++){
+//    for (int i = 1 ; i <=5 ; i++){
+//         cout<<i;
+//     }
+//           cout<<endl;
+// }
+
+
+
+// for(int j =1 ; j<=5 ; j++){
+//   for (char i = 'a' ; i<='e'; i++){
+//     cout<<i <<" ";
+// }
+// cout<<endl;
+// }
+
+// for(int i = 10 ; i > 0; i--){
+//     cout<<i<<" ";
+// }
+
+
+
+// for (int j = 0 ; j<= 5 ; j ++){
+
+//       for (int i = 0 ; i <= 5 ; i ++){
+//                 cout<<"*"<<" ";
+//       }
+
+//      cout<<endl;
+
+// }
+
+// for (int j = 1 ; j <= 5 ; j++){
+//         for(int i = 1 ; i <=j ; i++){
+//              cout<<"*"<<" ";
+//       }
+//    cout<<endl;   
+// }
+
+
+// for (int i = 5 ; i >=1; i--){
+//     for(int j = 1 ; j<=i ; j++){
+//         cout<<"*"<<" ";
+//     }
+//     cout<<endl;
+// }
+
+// for (int i = 5 ; i <)
+
+// for (int j = 0 ; j<4; j++){
+//     cout<<" ";
+// }
+// for (int j = 0 ; j < i; j++){
+//     cout<<"*";
+// }
+
+
+
+
+// for (int i = 0 ; i <=5 ; i ++){
+//     for (int j = 1 ; j <= (5-i);j++){
+//         cout<<" ";
+//     }
+//     for (int j = i ; j <= i ; j ++){
+//         cout<<"*";
+//     }
+// }
+
+
+//wrong 
+
+
+
+
+
+
+
+for (int i = 1 ; i < 5 ; i++){
+    for (int j = )
+}
+
+
+
+
+return 0 ;
+}
+
+
+
+
+
+
+
