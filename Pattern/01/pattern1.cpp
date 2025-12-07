cea46f6415cf662538e55f9822c58fcad46eeeed
CHANDAN <kumardalaichandan803@gmail.com>
2025-12-07 23:44:17 +0530

    Add files via upload

---
+++ Pattern/01/pattern1.cpp
@@ -0,0 +1,127 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int row,col;
+    
+
+
+    //1
+    // for(row=1;row<=5;row++){
+    //     for(col=1;col<=5;col++){
+    //         cout<<"*"<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //2
+    // for(row=1;row<=5;row++){
+    //     for(col=1;col<=5;col++){
+    //         cout<<"10"<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    //3
+    // for(row=1;row<=5;row++){
+    //     for(col=1;col<=5;col++){
+    //         cout<<row<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+    //4
+    // for(row=1;row<=5;row++){
+    //     for(col=1;col<=5;col++){
+    //         cout<<col<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    
+
+     
+    //5
+    // for(row=1;row<=5;row++){
+    //     for(col=5;col<=1;col++){
+    //         cout<<col<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // for(row=1;row<=5;row++){
+    //     for(col=5;col>=1;col--){
+    //         cout<<col<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+
+
+    //  for(row=1;row<=5;row++){
+    //     for(col=1;col<=5;col++){
+    //         cout<<(col*col)<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // cout<<endl;
+    //  for(row=1;row<=5;row++){
+    //     for(col=1;col<=5;col++){
+    //         cout<<(col*col*col)<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+
+    // for (row=1 ; row <= 5 ; row++){
+    //     char name = 'a'+(row-1);
+    //     for(col = 1 ; col <=5 ; col++){
+    //         cout<<name<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //  for(row=1;row<=5;row++){
+    //     char name;
+    //     for(name='a';name<='e';name++){
+    //         cout<<name<<" ";
+    //     }
+    //     cout<<endl;
+    //  }
+
+
+
+
+    int count = 1;
+    for(row= 1 ; row<=5 ; row++){
+        for(col=1;  col <=5 ; col++){
+            cout<<count<<" ";
+            
+        }
+        count++;
+        cout<<endl;
+    }
+    // int count = 1;
+    // for(row= 1 ; row<=5 ; row++){
+    //     for(col=1;  col <=5 ; col++){
+    //         cout<<count<<" ";
+    //         count++;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+
+
+
+
+
+    return 0;
+}
