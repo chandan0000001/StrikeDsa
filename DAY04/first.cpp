424cc033c8d15ad1950b08d7735647da6070cfef
CHANDAN <kumardalaichandan803@gmail.com>
2025-12-08 21:58:48 +0530

    soon adding otherwise see the pattern folder

---
+++ DAY04/first.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std; 
+int main(){
+
+
+
+
+
+
+
+
+
+
+    // for (int j=1 ; j<=5 ; j++){
+    //     bool num = j%2;
+    //     for(int i = 0; i<j; i++){
+    //         cout<<num;
+    //         num=!num;
+    //     }
+    //     cout<<endl;
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //  char name = 'a';
+    // for(char i = 'A' ; i<'B' ; i++){
+    //     cout<<i;
+    // }
+    // cout<<endl;
+    // for(char i = 'A' ; i<'C' ; i++){
+    //     cout<<i;
+    // }
+    // cout<<endl;
+    // for(char i = 'A' ; i<'D' ; i++){
+    //     cout<<i;
+    // }
+    // cout<<endl;
+    // for(char i = 'A' ; i<'E' ; i++){
+    //     cout<<i;
+    // }
+    // cout<<endl;
+
+
+
+    
+ 
+    return 0;  
+}
