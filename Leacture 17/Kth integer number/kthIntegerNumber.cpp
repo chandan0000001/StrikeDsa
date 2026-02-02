900dddbdcf85383529191f493484d102c1f70c15
chandan0000001 <kumardalaichandan803@gmail.com>
2026-02-02 19:48:23 +0530

    Hello there

--- Leacture 17/Kth integer number/kthIntegerNumber.cpp
+++ Leacture 17/Kth integer number/kthIntegerNumber.cpp
@@ -37,6 +37,18 @@ int KthMissingPositiveNumber(vector<int>& arr,int k){
     // }
     // return -1;
 
+	int n = arr.size()-1;
+	int start=0,end=n,ans=arr.size();
+	while(start<=end){
+		int mid=start+(end-start)/2;
+		if(arr[mid]-mid-1>=k){
+			ans=mid;
+			end=mid-1;
+		}else{
+			start=mid+1;
+		}
+	}
+	return ans+k;
 
 
 
