793ae2171b33af9f7ca2ca2e2b000c3218f6b655
chandan0000001 <kumardalaichandan803@gmail.com>
2026-02-22 20:16:39 +0530

    hello man this sunday but i am cooking

---
+++ Leacture 21/MaximumDifferenceOfTwoElements/maximumDifferenceOfTwoElements.cpp
@@ -0,0 +1,20 @@
+// 42. Maximum Difference between Two Elements
+
+// Given an array of integers, find the maximum difference between any two elements such that the larger element appears after the smaller element in the array.
+
+// Return the maximum difference found. If no such pair exists (i.e., the array is in non-increasing order), return -1.
+
+// Example 1:
+// Input: arr = [2, 3, 10, 6, 4, 8, 1]
+// Output: 8
+// Explanation:
+// The maximum difference is between 10 and 2.
+// Example 2:
+// Input: arr = [7, 9, 5, 6, 3, 2]
+// Output: 2
+// Explanation:
+// The maximum difference is between 9 and 7.
+
+// above analysis 
+//time complexity > O(n)
+//Space complexity > O(n)
