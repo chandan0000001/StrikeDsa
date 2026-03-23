52110b665d5c5fce9d68249aea38f719ba5c6877
Chandan Kumar Dalai <kumardalaichandan803@gmail.com>
2026-03-23 21:52:07 +0530

    exam is there so no progress

---
+++ Leacture 27/KMP_Algorithms/LongestPrefixSuffix.cpp
@@ -0,0 +1,26 @@
+// 67. Longest Prefix Suffix
+
+
+// Given a string s, of lowercase english alphabets, find the length of the longest proper prefix which is also a suffix .
+// Note: Prefix and suffix can be overlapping but they should not be equal to the entire string.
+
+// Example 1:
+// Input: s = "abab"
+// Output: 2
+// Explanation:
+// Find longest prefix that is also a suffix.
+// In "abab", the prefix "ab" matches the suffix "ab".
+// Length = 2.
+// So k = 2.
+// Example 2:
+// Input: s = "aabcdaabc"
+// Output: 4
+// Explanation:
+// Find longest prefix that is also a suffix.
+// In "aabcdaabc", the prefix "aabc" matches the suffix "aabc".
+// Length = 4.
+// So k = 4
+// Constraints:
+// • 1 <= n <= 10⁵ (string length)
+// • Return length of the longest substring, not the substring itself
+// • If no valid substring with exactly k distinct characters exists, return -1
