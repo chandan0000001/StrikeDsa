d8051f08d4cacb4525e4d60342f4f01993f4583a
Chandan Kumar Dalai <kumardalaichandan803@gmail.com>
2026-03-28 21:27:53 +0530

    hello pointer i hate you

---
+++ Leacture 30/deepPointer/pointerDeep.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+//  int *p = new int[10];
+//  //new array creaete dynamically 
+//  for(int i=0;i<10;i++){
+//     // cin>>p[i];
+//     // p[i]=i*5;//or we can write same as per the below 
+//     *(p+i)=i*5;
+//  }
+// for(int i=0;i<10;i++){
+//     cout<<p[i]<<endl;
+// }
+// //free the memoery 
+// delete[] p;
+
+
+// now create a double pointer -> genrate a 2D array
+
+   return 0; 
+}
