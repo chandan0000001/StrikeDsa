3466f8731634e21f0a9bd8add96603051df67f92
Chandan Kumar Dalai <kumardalaichandan803@gmail.com>
2026-05-03 20:50:18 +0530

    Hello

--- REVISION/3-5-26/LinkedListCycle.cpp
+++ REVISION/3-5-26/LinkedListCycle.cpp
@@ -4,6 +4,34 @@
 
 // GG               https://www.geeksforgeeks.org/problems/detect-loop-in-linked-list/1
 
+/*
+class Node {
+   public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+} */
+
+// class Solution {
+//   public:
+//     bool detectLoop(Node* head) {
+//         // code here
+//         //base case 
+//         if(!head || !head->next) { return false;}
+//         Node*slow = head , *fast = head;
+//         while(fast && fast->next){
+           
+//             slow=slow->next;
+//             fast=fast->next->next; 
+//             if(slow==fast) {return true;}
+//         }
+//         return false;
+//     }
+// };
 
 
 
