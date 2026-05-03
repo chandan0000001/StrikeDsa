516739689dac8e43c9811b5c69eb132297a7609d
Chandan Kumar Dalai <kumardalaichandan803@gmail.com>
2026-05-03 20:50:58 +0530

    CycleLinkedlist

--- REVISION/3-5-26/LinkedListCycle.cpp
+++ REVISION/3-5-26/LinkedListCycle.cpp
@@ -1,5 +1,26 @@
 // LEETCODE      https://leetcode.com/problems/linked-list-cycle/?envType=problem-list-v2&envId=linked-list
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) 
+//     { if(!head || !head->next){return false;}
+//        ListNode *slow=head,*fast = head->next;
+      
+//        while(fast && fast->next){
+//         slow=slow->next;
+//         fast=fast->next->next;
+//         if(slow == fast) {return true;}
+//        } 
+//        return false;
+//     }
+// };
 
 
 // GG               https://www.geeksforgeeks.org/problems/detect-loop-in-linked-list/1
