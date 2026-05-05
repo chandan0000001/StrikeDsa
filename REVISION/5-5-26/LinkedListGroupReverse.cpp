8f5d0fd565edbb7eb816326bd8d32e7ecf957677
Chandan Kumar Dalai <kumardalaichandan803@gmail.com>
2026-05-05 22:47:06 +0530

    Reverse LinkedList in group

---
+++ REVISION/5-5-26/LinkedListGroupReverse.cpp
@@ -0,0 +1,24 @@
+// 345. Linked List Group Reverse
+// HARD
+// Solved
+// You are given the head of a Singly linked list. You have to reverse every k nodes in the linked list and return the head of the modified list.
+
+// Note: If the number of nodes is not a multiple of k, then the left-out nodes at the end should be considered as a group and must be reversed as well.
+
+// Example 1:
+// Example 1 image 1
+// Input: head =[1, 2, 3, 4, 5, 6], k = 2
+// Output: [2, 1, 4, 3, 6, 5]
+// Explanation:
+// Linked List is reversed in a group of size k = 2.
+// Example 2:
+// Input: head =[1, 2, 3, 4, 5, 6], k = 4
+// Output: [4, 3, 2, 1, 6, 5]
+// Explanation:
+// Linked List is reversed in a group of size k = 4.
+// Constraints:
+// • 1 ≤ size of linked list ≤ 10^5
+// • 0 ≤ node->data ≤ 10^6
+// • 1 ≤ k ≤ size of linked list
+
+
