4280ee357161c9791a1a82f11d96ea400dd53a86
Chandan Kumar Dalai <kumardalaichandan803@gmail.com>
2026-06-08 23:50:03 +0530

    Strike DSA

--- Leacture 46/stackPrrobleam/nextgraterElement.cpp
+++ Leacture 46/stackPrrobleam/nextgraterElement.cpp
@@ -33,3 +33,23 @@
 // • 1 ≤ T ≤ 100
 // • 1 ≤ N ≤ 10⁵
 // • 1 ≤ arr[i] ≤ 10⁷
+
+
+
+
+
+// vector<int> nextLargerElement(vector<int>& arr) {
+//     stack<int>st;
+//     vector<int>ans(arr.size(),-1); //initlize by -1 
+//     for(int i =0 ; i<arr.size();i++){
+//        while(!st.empty() && arr[i]>arr[st.top()]){
+//            ans[st.top()]=arr[i];
+//            st.pop();
+   
+//        }
+//        st.push(i);
+//     }   
+//     return ans;
+//    }
+   
+//    //TC O(n) sp -> o(N)  exta stack and vecotr 
